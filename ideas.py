df86d6369b80b4e46970f94a474402729308e2e4
Joseph Tessitore <joetgamerman3@gmail.com>
2022-10-08 12:58:19 -0400

    Initial (and most likely final) commit

---
+++ ideas.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from pathlib import Path
+import os
+
+# Folder for ideas
+IDEAS_FOLDER = Path(os.environ["HOME"]) / ".local/share/ideas/"
+
+def ideas_folder_exists():
+    return IDEAS_FOLDER.exists()
+
+def main():
+    title = input("What is your idea? ")
+
+    print("Interesting, tell me more...")
+    description = list()
+    line = "placeholder"
+    while line != "":
+        line = input("> ")
+        description.append(line)
+    
+    #path = IDEAS_FOLDER / f"{str(get_number())}.md"
+    path = IDEAS_FOLDER / f"{title}.md"
+    with path.open("w") as idea_file:
+        idea_file.write(f"# {title}\n")
+        for line in description:
+            idea_file.write(f"{line}\n")
+
+    print(f"Sounds like a fun idea! I recorded it at {path}")
+
+if __name__ == "__main__":
+    main()
